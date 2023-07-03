210c1940fe286b7908614af7efcdf4d0ba455e7d
ricarvsan <ricarvsan@gmail.com>
2023-07-03 13:38:45 -0300

    post /messages

--- src/app.js
+++ src/app.js
@@ -17,10 +17,10 @@ dotenv.config();
 const mongoClient = new MongoClient(process.env.DATABASE_URL);
 
 try {
-    await mongoClient.connect();
-    console.log('MongoDB conectado!')
+  await mongoClient.connect();
+  console.log('MongoDB conectado!')
 } catch (err) {
-    err => console.log(err.message)
+  err => console.log(err.message)
 }
 
 const db = mongoClient.db();
@@ -28,35 +28,66 @@ const db = mongoClient.db();
 // FUNÇÕES (ENDPOINTS)
 app.post('/participants', async (req, res) => {
 
-    const {name} = req.body;
+  const { name } = req.body;
 
-    const nameSchema = Joi.object({
-      name: Joi.string().required(),
-    });
-  
-    const validation = nameSchema.validate(req.body, {abortEarly: false});
-    if(validation.error) {
-      const err = validation.error.details.map(detail => detail.message);
-      return res.status(422).send(err);
-    }
+  const nameSchema = Joi.object({
+    name: Joi.string().required(),
+  });
 
-    try {
-      const user = await db.collection('participants').findOne({name});
-      if(user) return res.status(409).send("Esse usuário já existe na sala!")
+  const validation = nameSchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    const err = validation.error.details.map(detail => detail.message);
+    return res.status(422).send(err);
+  }
 
-      await db.collection('participants').insertOne({name, lastStatus: Date.now()});
+  try {
+    const user = await db.collection('participants').findOne({ name });
+    if (user) return res.status(409).send("Esse usuário já existe na sala!")
 
-      await db.collection('messages').insertOne({from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')});
-      res.sendStatus(201);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
+    await db.collection('participants').insertOne({ name, lastStatus: Date.now() });
+
+    await db.collection('messages').insertOne({ from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss') });
+    res.sendStatus(201);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 });
 
 app.get('/participants', async (req, res) => {
   try {
     const participants = await db.collection('participants').find().toArray();
     res.send(participants);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.post('/messages', async (req, res) => {
+
+  const { to, text, type } = req.body;
+  const { user } = req.headers;
+
+  const messageSchema = Joi.object({
+    to: Joi.string().required(),
+    text: Joi.string().required(),
+    type: Joi.string().valid('message', 'private_message').required()
+  });
+
+  const validation = messageSchema.validate(req.body, { abortEarly: false });
+  if (validation.error) {
+    const err = validation.error.details.map(details => details.message);
+    return res.status(422).send(err);
+  }
+
+  const msg = { from: user, to, text, type, time: dayjs().format('HH:mm:ss') };
+
+  try {
+    const logged = await db.collection('participants').findOne({ name: user });
+    if(!logged) return res.sendStatus(422);
+
+    await db.collection('messages').insertOne(msg);
+
+    res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
   }
@@ -70,37 +101,6 @@ app.get('/messages', async (req, res) => {
     res.status(500).send(err.message);
   }
 });
-
-app.post('/messages', async (req, res) => {
-
-  const {to, text, type} = req.body;
-  const {user} = req.headers;
-
-  res.sendStatus(201)
-
-  const messageSchema = Joi.object({
-    to: Joi.string().required(),
-    text: Joi.string().required(),
-    type: Joi.string().required('private_message' || 'message')
-  });
-
-  const validation = messageSchema.validate({abortEarly: false});
-  if(validation.error) {
-    const err = validation.error.details.map(details => details.message);
-    return res.status(422).send(err);
-  }
-
-  const msg = {from: user, to, text, type, time: Date.now()};
-  res.send(msg)
-
-  /* try {
-    console.log(msg)
-    res.sendStatus(201);
-  } catch (err) {
-    res.status(500).send(err.message);
-  } */
-});
-
 
 // LIGAR APP DO SERVER PARA OUVIR REQUISIÇÕES
 const PORT = 5000;
